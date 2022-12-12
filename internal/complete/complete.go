09f59d2397932387b375037f3e71dc215d35fa3f
Alastair Flynn <alastairflynn@hotmail.co.uk>
2022-12-12 17:08:07 +0000

    Add complete package

---
+++ internal/complete/complete.go
@@ -0,0 +1,51 @@
+package complete
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/canonical/sqlair/internal/assemble"
+	"github.com/canonical/sqlair/internal/parse"
+	"github.com/canonical/sqlair/internal/typeinfo"
+)
+
+type colInfo struct {
+	columns    []string
+	values     []any
+	valuePtrs  []any
+	colToIndex map[string]int
+}
+
+type CompletedExpr struct {
+	Parsed      *parse.ParsedExpr
+	InputValues []any
+}
+
+type typeNameToValue = map[string]any
+
+func Complete(ae *assemble.AssembledExpr, args ...any) (*CompletedExpr, error) {
+	var tv = make(typeNameToValue)
+	for _, arg := range args {
+		if arg == (any)(nil) {
+			return nil, fmt.Errorf("cannot reflect nil value")
+		}
+		tv[reflect.TypeOf(arg).Name()] = arg
+	}
+
+	fvs := []any{}
+
+	for _, part := range ae.Parsed.QueryParts {
+		if p, ok := part.(*parse.InputPart); ok {
+			if v, ok := tv[p.Source.Prefix]; ok {
+				fv, err := typeinfo.GetField(v, p.Source)
+				if err != nil {
+					return nil, err
+				}
+				fvs = append(fvs, fv)
+			}
+			return nil, fmt.Errorf("type %s not passed as an arguement", p.Source.Prefix)
+		}
+	}
+
+	return &CompletedExpr{ae.Parsed, fvs}, nil
+}
