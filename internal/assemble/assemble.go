0c5897654a3237902758726bc07bddd78a4c0267
Fernando Apesteguia <fernando.apesteguia@gmail.com>
2022-12-15 14:09:09 +0100

    Fix column creation.
    And update tests from main parse-dev branch.

--- internal/assemble/assemble.go
+++ internal/assemble/assemble.go
@@ -1,6 +1,7 @@
 package assemble
 
 import (
+	"bytes"
 	"fmt"
 	"sort"
 	"strings"
@@ -16,9 +17,8 @@ type AssembledExpr struct {
 
 type typeNameToInfo map[string]*typeinfo.Info
 
-// assembleInput checks that the type in input expression is one we have
-// reflected on and that the tag exists.
 func assembleInput(ti typeNameToInfo, p *parse.InputPart) error {
+
 	if inf, ok := ti[p.Source.Prefix]; ok {
 		if _, ok := inf.TagToField[p.Source.Name]; ok {
 			return nil
@@ -29,62 +29,44 @@ func assembleInput(ti typeNameToInfo, p *parse.InputPart) error {
 	return fmt.Errorf("unknown type: %s", p.Source.Prefix)
 }
 
-// makeOutCols builds an array of columns out of an array of tags.
-// It admits a prefix parameter that will be prepended to the column name.
-func makeOutCols(tags []string, prefix string) []string {
-	var outCols []string = make([]string, 0)
-	for _, tag := range tags {
-		outCols = append(outCols, prefix+tag)
+type sourceField int
+
+const (
+	fromString sourceField = iota
+	fromName
+)
+
+// createOutCols creates the list of output columns from the parameters passed
+// in.
+// The source and sf define the field to use from the FullName. If inf is not
+// nil, then it is used to run some checks.
+// Returns the array of columns and nil on success, nil and an error otherwise.
+func createOutCols(source []parse.FullName, sf sourceField, inf *typeinfo.Info) ([]string, error) {
+	var outCols []string
+	for _, c := range source {
+		if inf != nil {
+			if _, ok := inf.TagToField[c.Name]; !ok {
+				return nil, fmt.Errorf("there is no tag with name %s in %s",
+					c.Name, inf.Type.Name())
+			}
+		}
+		if sf == fromString {
+			outCols = append(outCols, c.String())
+		} else {
+			outCols = append(outCols, c.Name)
+		}
 	}
-	return outCols
+	return outCols, nil
 }
 
 func assembleOutput(ti typeNameToInfo, p *parse.OutputPart) ([]string, error) {
 
 	var outCols []string = make([]string, 0)
 
-	// In parse we ensure that if p.Target[0] is a * then len(p.Target) == 1
-	// Case 1: Star target cases e.g. ...&P.*
-	if p.Target[0].Name == "*" {
-		var tags []string
-
-		inf, ok := ti[p.Target[0].Prefix]
-		if !ok {
-			return nil, fmt.Errorf("unknown type: %s", p.Target[0].Prefix)
-		}
-		for tag := range inf.TagToField {
-			tags = append(tags, tag)
-		}
-
-		if len(p.Source) > 0 { // Star with AS
-			if p.Source[0].Name == "*" { // Single star column e.g. t.* AS &P.*
-				pref := ""
-				if p.Source[0].Prefix != "" {
-					pref = p.Source[0].Prefix + "."
-				}
-				outCols = makeOutCols(tags, pref)
-			} else { // Explicit columns e.g. (col1, col2) AS &P.*
-				for _, c := range p.Source {
-					if _, ok := inf.TagToField[c.Name]; !ok {
-						return nil, fmt.Errorf("there is no tag with name %s in %s",
-							c.Name, inf.Type.Name())
-					}
-					outCols = append(outCols, c.String())
-				}
-			}
-		} else { // This is the case for star but no columns e.g. &P.*
-			outCols = makeOutCols(tags, "")
-		}
-		// The strings are sorted to give a deterministic order for
-		// testing.
-		sort.Strings(outCols)
-		return outCols, nil
-	}
-
-	// Case 2: None star target cases e.g. ...&(P.name, P.id)
+	// Check target type and tags are valid.
 	for _, t := range p.Target {
 		if inf, ok := ti[t.Prefix]; ok {
-			if _, ok := inf.TagToField[t.Name]; !ok {
+			if _, ok := inf.TagToField[t.Name]; !ok && t.Name != "*" {
 				return nil, fmt.Errorf("there is no tag with name %s in %s",
 					t.Name, inf.Type.Name())
 			}
@@ -92,16 +74,44 @@ func assembleOutput(ti typeNameToInfo, p *parse.OutputPart) ([]string, error) {
 			return nil, fmt.Errorf("unknown type: %s", t.Prefix)
 		}
 	}
-	if len(p.Source) > 0 { // Explicit columns e.g. name_1 AS P.name
-		for _, c := range p.Source {
-			outCols = append(outCols, c.String())
+
+	// Case 1: Star target cases e.g. "...&P.*".
+	// In parse we ensure that if p.Target[0] is a * then len(p.Target) == 1
+	if p.Target[0].Name == "*" {
+
+		inf, _ := ti[p.Target[0].Prefix]
+
+		// Case 1.1: Single star e.g. "t.* AS &P.*" or "&P.*"
+		if (len(p.Source) > 0 && p.Source[0].Name == "*") ||
+			len(p.Source) == 0 {
+			pref := ""
+			if len(p.Source) > 0 && p.Source[0].Prefix != "" {
+				pref = p.Source[0].Prefix + "."
+			}
+			for tag := range inf.TagToField {
+				outCols = append(outCols, pref+tag)
+			}
+			// The strings are sorted to give a deterministic order for
+			// testing.
+			sort.Strings(outCols)
+			return outCols, nil
 		}
-	} else { // No columns e.g. &(P.name, P.id)
-		for _, t := range p.Target {
-			outCols = append(outCols, t.Name)
+
+		// Case 1.2: Explicit columns e.g. "(col1, t.col2) AS &P.*".
+		if len(p.Source) > 0 {
+			return createOutCols(p.Source, fromString, inf)
 		}
 	}
-	return outCols, nil
+
+	// Case 2: None star target cases e.g. "...&(P.name, P.id)".
+
+	// Case 2.1: Explicit columns e.g. "name_1 AS P.name".
+	if len(p.Source) > 0 {
+		return createOutCols(p.Source, fromString, nil)
+	}
+
+	// Case 2.2: No columns e.g. "&(P.name, P.id)".
+	return createOutCols(p.Target, fromName, nil)
 }
 
 func Assemble(pe *parse.ParsedExpr, args ...any) (expr *AssembledExpr, err error) {
@@ -122,16 +132,16 @@ func Assemble(pe *parse.ParsedExpr, args ...any) (expr *AssembledExpr, err error
 		ti[i.Type.Name()] = i
 	}
 
-	sql := ""
+	var sql bytes.Buffer
 
-	// Check and expand each query part
+	// Check and expand each query part.
 	for _, part := range pe.QueryParts {
 		if p, ok := part.(*parse.InputPart); ok {
 			err := assembleInput(ti, p)
 			if err != nil {
 				return nil, err
 			}
-			sql = sql + p.ToSQL([]string{})
+			sql.WriteString(p.ToSQL([]string{}))
 			continue
 		}
 
@@ -140,16 +150,16 @@ func Assemble(pe *parse.ParsedExpr, args ...any) (expr *AssembledExpr, err error
 			if err != nil {
 				return nil, err
 			}
-			sql = sql + p.ToSQL(outCols)
+			sql.WriteString(p.ToSQL(outCols))
 			continue
 		}
 
 		p := part.(*parse.BypassPart)
-		sql = sql + p.ToSQL([]string{})
+		sql.WriteString(p.ToSQL([]string{}))
 
 	}
 
-	sql = strings.TrimSpace(sql)
-	// We will probably need to save the outcols and in cols
-	return &AssembledExpr{Parsed: pe, SQL: sql}, nil
+	sqlString := strings.TrimSpace(sql.String())
+	// We will probably need to save the outcols and in cols.
+	return &AssembledExpr{Parsed: pe, SQL: sqlString}, nil
 }
